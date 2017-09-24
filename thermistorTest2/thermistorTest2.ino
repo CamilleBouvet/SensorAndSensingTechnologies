b64e722e841b05c3c718c8d51275be3a4dca0dc8
CamilleBouvet <camille_bouvet@hotmail.fr>
2017-09-24 23:33:51 +0800

    AddOfThermisterHomework

---
+++ thermistorTest2/thermistorTest2.ino
@@ -0,0 +1,28 @@
+#include <math.h>
+
+double Thermistor(int RawADC) {
+   double Temp;
+   Temp = 0.1236*RawADC - 34.064;
+    return Temp;
+}
+  
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    pinMode(A0, INPUT);
+
+}
+
+int compte = 0;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print(analogRead(A0));
+  Serial.print("  Measure ");
+  Serial.print(int(compte));
+  Serial.print(" : ");
+  Serial.println(int (Thermistor(analogRead(0))));  // display Celsius
+  compte++;
+  delay(5000);
+
+}
