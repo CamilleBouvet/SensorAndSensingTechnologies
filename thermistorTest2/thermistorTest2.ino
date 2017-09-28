fe256cf5d4092575a4ab216e0473d64e6cd60f05
CamilleBouvet <camille_bouvet@hotmail.fr>
2017-09-28 11:50:37 +0800

    Arduino

--- thermistorTest2/thermistorTest2.ino
+++ thermistorTest2/thermistorTest2.ino
@@ -1,9 +1,22 @@
 #include <math.h>
 
-double Thermistor(int RawADC) {
+double tableAandB[7][3] = 
+{
+    {22,1.5008,-78.368},
+    {352,0.1526,-37.131},
+    {463,0.1632,-50.514},
+    {771,0.1164,17.872},
+    {925,0.2507,54.314},
+    {1012,1.0971,82.831},
+    {1023,21.267,163}
+    };
+
+double Thermistor(int RawADC, double a, double b) {
    double Temp;
-   Temp = 0.1236*RawADC - 34.064;
-    return Temp;
+   Temp = a*RawADC + b;
+   Serial.println(a);
+   Serial.println(b);
+   return Temp;
 }
   
 void setup() {
@@ -14,14 +27,28 @@ void setup() {
 }
 
 int compte = 0;
+int i = 0;
+bool valueFind = true;
+int vOut = 0;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print(analogRead(A0));
+  i = -1;
+  valueFind = true;
+  vOut = analogRead(A0);
+  Serial.print(int(vOut));
   Serial.print("  Measure ");
   Serial.print(int(compte));
   Serial.print(" : ");
-  Serial.println(int (Thermistor(analogRead(0))));  // display Celsius
+  while((i<=6) && (valueFind))
+  {
+    i++;
+    if(vOut <= tableAandB[i][0])
+    {
+      valueFind = false;
+      Serial.println(i);
+    }
+  }
+  Serial.println(int (Thermistor(vOut,tableAandB[i][1], tableAandB[i][2])));  // display Celsius
   compte++;
   delay(5000);
 
